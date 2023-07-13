ece06d8bf467f82e9384a485691e86e4211766d2
석훈0020 <seokhun0020@gmail.com>
2023-07-13 10:36:50 +0900

    등록

---
+++ src/test/java/com/jojodu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojodu.book.freelecspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
